971f1c2c95f3327c25110c6c5665842268f13a8e
juliapim <juliarpim@gmail.com>
2023-01-13 13:04:41 +0100

    arrange the code

--- src/App.js
+++ src/App.js
@@ -11,28 +11,27 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-     
          <section className="App-container2">
-         <div className='liv-logo'>
-         <img src={livlogo} className="App-logo" alt="logo" />
-         </div>
-         <div>
-          <h1>bringing you serenity, joy and hydration</h1>
+          <div className='liv-logo'>
+           <img src={livlogo} className="App-logo" alt="logo" />
+          </div>
+          <div>
+           <h1>bringing you serenity, joy and hydration</h1>
           </div>
          </section>
          <section className="App-container">
-         <div>
+          <div>
             <h4>Three different flavors for your instant happiness</h4>
-        </div>
-          <ProductInfo/>
+         </div>
+        <ProductInfo/>
         </section>
         <section className="App-container3">
-        <ViewSwitch/>
-          <div className='product-picture'>
-            <img className='tea-shadow' src={teaImage} alt="tea-shadow"></img>
-          </div> 
+         <ViewSwitch/>
+         <div className='product-picture'>
+          <img className='tea-shadow' src={teaImage} alt="tea-shadow"></img>
+         </div> 
         </section>
-     <Footer />
+         <Footer />
     </div>
   );
 }
